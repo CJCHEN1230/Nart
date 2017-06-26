300b9af7f0b0f4e2385f38244cbea835165e3c49
Adam <Adam@DESKTOP-H9T8C51>
2017-06-26 17:00:34 +0800

    鏡心位置計算成功，(注意其load旋轉矩陣校正檔案方式不同)

--- Nart/CalcCoord.cs
+++ Nart/CalcCoord.cs
@@ -11,56 +11,46 @@ namespace Nart
     public class CalcCoord
     {
 
-        public  Matrix3D exParam;
+        private CamParam[] _camParam = new CamParam[2];
 
-        public double FocalLength;
-        public double Kappa1;
-        public double Cx;
-        public double Cy;
-        public double Sx;
+        public Point4D[] LensCenter = new Point4D[2];
+
 
         public CalcCoord()
         {
-            
-            
+            _camParam[0] = new CamParam("../../../data/CaliR_L.txt");
+            _camParam[1] = new CamParam("../../../data/CaliR_R.txt");
+            CalcLensCenter();
         }
-        public void  LoadCalibrationFile()
+        
+        private void CalcLensCenter()
         {
-            string fileContent = File.ReadAllText("../../../data/CaliR_L.txt");
-            string[] contentArray = fileContent.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
+            LensCenter[0] = new Point4D(0, 0, 0, 1);
+            LensCenter[1] = new Point4D(0, 0, 0, 1);
+        
+            //Console.WriteLine("Rotation:\n" +
+            //    _camParam[0].Rotation.M11 + "\t" + _camParam[0].Rotation.M12 + "\t" + _camParam[0].Rotation.M13 + "\t" + _camParam[0].Rotation.M14 + "\n" +
+            //    _camParam[0].Rotation.M21 + "\t" + _camParam[0].Rotation.M22 + "\t" + _camParam[0].Rotation.M23 + "\t" + _camParam[0].Rotation.M24 + "\n" +
+            //    _camParam[0].Rotation.M31 + "\t" + _camParam[0].Rotation.M32 + "\t" + _camParam[0].Rotation.M33 + "\t" + _camParam[0].Rotation.M34 + "\n" +
+            //    _camParam[0].Rotation.OffsetX + "\t" + _camParam[0].Rotation.OffsetY + "\t" + _camParam[0].Rotation.OffsetZ + "\t" + _camParam[0].Rotation.M44 + "\n");
+
+            //Console.WriteLine("Translation invert之前:\n" +
+            //    _camParam[0].Translation.M11 + "\t" + _camParam[0].Translation.M12 + "\t" + _camParam[0].Translation.M13 + "\t" + _camParam[0].Translation.M14 + "\n" +
+            //    _camParam[0].Translation.M21 + "\t" + _camParam[0].Translation.M22 + "\t" + _camParam[0].Translation.M23 + "\t" + _camParam[0].Translation.M24 + "\n" +
+            //    _camParam[0].Translation.M31 + "\t" + _camParam[0].Translation.M32 + "\t" + _camParam[0].Translation.M33 + "\t" + _camParam[0].Translation.M34 + "\n" +
+            //    _camParam[0].Translation.OffsetX + "\t" + _camParam[0].Translation.OffsetY + "\t" + _camParam[0].Translation.OffsetZ + "\t" + _camParam[0].Translation.M44 + "\n");
+
+
+            _camParam[0].extParam.Invert();
+            _camParam[1].extParam.Invert();
+
+            LensCenter[0] = _camParam[0].extParam.Transform(LensCenter[0]);
+            LensCenter[1] = _camParam[1].extParam.Transform(LensCenter[1]);
             
-            string[] data = new string[20];
-            Array.Copy(contentArray, 3, data, 0, 20);
-
-            double[] cameraData = Array.ConvertAll(data, double.Parse);
-
-            
-
-            exParam = new Matrix3D(cameraData[0], cameraData[1], cameraData[2], 0,
-                                   cameraData[3], cameraData[4], cameraData[5], 0,
-                                   cameraData[6], cameraData[7], cameraData[8], 0,
-                                   cameraData[9], cameraData[10], cameraData[11], 1);
-
-
-            FocalLength = cameraData[12];						
-            Kappa1 = cameraData[13]; 
-            Cx = cameraData[14]; 
-            Cy = cameraData[15];
-            Sx = cameraData[19];
-
-
-
-            Console.WriteLine(exParam);
-            Console.WriteLine(FocalLength);
-            Console.WriteLine(Kappa1);
-            Console.WriteLine(Cx);
-            Console.WriteLine(Cy);
-            Console.WriteLine(Sx);
-
-            Console.Read();
 
 
         }
-
     }
+
+    
 }
---
+++ Nart/CamParam.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Media3D;
+
+namespace Nart
+{
+    class CamParam
+    {
+        public Matrix3D extParam; //exteranl parameters
+        
+        public double FocalLength;
+        public double Kappa1;
+        public double Cx;
+        public double Cy;
+        public double Sx;
+
+        public double dx;//0.00375
+        public double dy;//0.00375
+        public double Ncx;//1296
+        public double Nfx;//1280
+        public double dpx;
+
+        public CamParam(string path)
+        {
+            dx = 0.0044;//0.00375
+            dy = 0.0044;//0.00375
+            Ncx = 1628;//1296
+            Nfx = 1600;//1280
+            dpx = dx * Ncx / Nfx;
+            LoadCalibrationFile(path);
+        }
+
+
+        public void LoadCalibrationFile(string path)
+        {
+            try
+            {
+                string fileContent = File.ReadAllText(path);//"../../../data/CaliR_L.txt"
+                string[] contentArray = fileContent.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
+
+                string[] data = new string[20];
+                Array.Copy(contentArray, 3, data, 0, 20);
+
+                double[] cameraData = Array.ConvertAll(data, double.Parse);
+               
+                extParam = new Matrix3D(cameraData[0], cameraData[3], cameraData[6], 0,
+                                       cameraData[1], cameraData[4], cameraData[7], 0,
+                                       cameraData[2], cameraData[5], cameraData[8], 0,
+                                       cameraData[9], cameraData[10], cameraData[11], 1);
+
+             
+                FocalLength = cameraData[12];
+                Kappa1 = cameraData[13];
+                Cx = cameraData[14];
+                Cy = cameraData[15];
+                Sx = cameraData[19];
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("校正檔案路徑錯誤");
+            }
+
+            Console.WriteLine(extParam);
+            Console.WriteLine(FocalLength);
+            Console.WriteLine(Kappa1);
+            Console.WriteLine(Cx);
+            Console.WriteLine(Cy);
+            Console.WriteLine(Sx);
+            Console.WriteLine("\n\n\n");
+            //Console.Read();
+
+
+        }
+
+    }
+}
