0f21c161fab0bd28238bd5d798fada8e2cad4954
Adam <pipited02@gmail.com>
2017-09-13 20:38:04 +0800

    test

--- NartControl/Control/RegistrationViewModel.cs
+++ NartControl/Control/RegistrationViewModel.cs
@@ -6,7 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NartControl.Control
-{
+{/// <summary>
+/// //////////////hellotest
+/// 
+/// </summary>
     class RegistrationViewModel : BaseViewModel
     {
         //private ObservableCollection<ModelData> userObservable;
